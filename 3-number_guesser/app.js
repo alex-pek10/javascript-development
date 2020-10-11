c34fa0eaffdeef133fa88d392ea567eba3184c1f
Alex Myers <alex@pek10.dev>
2020-10-11 00:22:40 -0400

    Establish game rules in comments

---
+++ 3-number_guesser/app.js
@@ -0,0 +1,8 @@
+/*
+  GAME FUNCTION:
+  - The player must guess a number between the min and max value
+  - The player gets a certain amount of guesses
+  - The game will notify the player of remaining guesses
+  - If the player loses, the game will reveal the correct number
+  - The player can choose to play again
+*/
