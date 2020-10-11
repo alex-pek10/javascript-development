fd10941287311a149e954699746ad27b897f0903
Alex Myers <alex@pek10.dev>
2020-10-10 23:50:17 -0400

    Format with Prettier (cleanup)

--- 2-loan_calculator/app.js
+++ 2-loan_calculator/app.js
@@ -24,7 +24,7 @@ function calculateResults(e) {
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
     showError('Please check your numbers');
   }
